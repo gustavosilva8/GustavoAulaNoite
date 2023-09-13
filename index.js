116c0c24c9793b3eedcc993bfced3963a547b810
gustavosilva8 <144861422+gustavosilva8@users.noreply.github.com>
2023-09-13 19:19:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+EXERCÍCIO 4
+let pokemonInicial = "Squirtle"
+if (pokemonInicial === "Bulbassauro"){
+    console.log("seu pokemon é do tipo planta e veneno")
+} else if (pokemonInicial === "Charmander"){
+    console.log("seu pokemon é do tipo fogo")
+} else {
+    console.log("seu pokemon é do tipo água")
+}
+*/
+/*
+let pokemonInicial = "Bulbassauro"
+switch (pokemonInicial) {
+    case `Squirtle`:
+        console.log("seu pokemon é do tipo água")
+        break
+        case `Charmander`:
+        console.log("seu pokemon é do tipo fogo")
+        break
+        case `Bulbassauro`:
+            console.log("seu pokemon é do tipo planta e veneno")
+            break
+            default:
+              console.log("pokemon não encontrado")
+}*/
+
+/*
+let concluiuEnsinoMédio = prompt("Você concluiu o ensino médio?")
+let maiorIdade = Number(prompt ("qual sua idade?"))>= 18
+let cursandoFaculdade = prompt("você cursa uma faculdade?")
+
+if (concluiuEnsinoMédio || maiorIdade || cursandoFaculdade){
+
+    console.log("Você pode cursar uma faculdade")
+} else {
+    console.log("Você não pode cursar uma faculdade")
+}*/
+/*
+    let concluiuEnsinoMédio = prompt("Você já concluiu o Ensino Médio")
+    let idade = Number(prompt("Qual a sua Idade?") >=18)
+    let cursandoFaculdade = prompt("Você já cursa Ensino Superior?")
+    
+
+ function verificaFacul (concluiuEnsinoMédio, maiorIdade, cursandoFaculdade) {
+    
+    if (concluiuEnsinoMédio === "sim" && maiorIdade && cursandoFaculdade === "não"){
+    
+        console.log("Você pode cursar uma faculdade")
+    } else {
+        console.log("Você não pode cursar uma faculdade")
+    }  
+
+ }
+  verificaFacul(concluiuEnsinoMédio, maiorIdade, cursandoFaculdade)
+*/
+
